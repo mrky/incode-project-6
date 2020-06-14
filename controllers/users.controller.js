dffcb1282d6f887a4057a6afb0d520a63b6e3056
anashibata <51089103+anashibata@users.noreply.github.com>
2020-06-15 00:08:27 +0100

    Add files via upload

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,43 +1,31 @@
-const UserSchema = require('../models/user.model');
+const UserSchema = require('../models/user.model')
 module.exports = {
     index: (req, res, next) => {
-        res.render('users/login', {
-            title: 'Login',
-        });
-    },
+        res.render('users/login')
+    }, 
 
     login: (req, res, next) => {
-        console.log('se' + req.body.password);
-        UserSchema.login(req.body.email, req.body.password)
-            .then(function (user) {
-                console.log('usu ' + user);
-                req.session.email = user.email;
-                req.session.isAdmin = user.isAdmin;
-                console.log('sessao ' + req.session.firstName);
-                res.render('index', { title: 'Login', loggedIn: true });
-            })
-            .catch((err) =>
-                setImmediate(() => {
-                    console.log(err);
-                    res.status(500).send(err.toString());
-                })
-            );
+        UserSchema.login(req.body.email, req.body.password).then(function(user) {   
+            req.session.email = user.email      
+            req.session.isAdmin = user.isAdmin 
+            res.render('index', {title:'Login', loggedIn : true})
+        }).catch((err) => setImmediate(() => {console.log(err); res.status(500).send(err.toString())}));   
     },
 
     displayRegister: (req, res, next) => {
-        // todo
-        res.send('respond with a resource');
+        res.render('users/register')
     },
 
     register: (req, res, next) => {
-        // todo
-        res.send('respond with a resource');
+        UserSchema.register(req.body).then(function(user) {    
+            res.render('users/login', {title:'Login', loggedIn : true})
+        }).catch((err) => setImmediate(() => {console.log(err); res.status(500).send(err.toString())}));   
     },
 
     logout: (req, res, next) => {
-        console.log('sessao ' + req.session.email);
+        console.log('sessao '+req.session.email)
         req.session.destroy(function (err) {
-            res.render('index', { loggedIn: false });
+            res.render('index', {isLogged : false})
         });
     },
 
