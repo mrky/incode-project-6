e4912447b9c70435e198e0ad092cf35298cde709
anashibata <51089103+anashibata@users.noreply.github.com>
2020-06-15 00:07:54 +0100

    Add files via upload

--- models/user.model.js
+++ models/user.model.js
@@ -15,7 +15,8 @@ const UserModel = new Schema({
     },
     email: {
         type: String,
-        require: true
+        require: true,
+        unique: true
     },
     password: {
         type: String,
@@ -28,19 +29,39 @@ const UserModel = new Schema({
     }
 })
 dbConnection.mongoose.model("users", UserModel)
+const User = dbConnection.mongoose.model('users')
 
-login = (email, password) => {    
-    const User = dbConnection.mongoose.model('users')    
+login = (email, password) => {
     return new Promise((resolve, reject) => {
         User.findOne({ email: email }).then((user) => {
-            if (user == null) { reject(new Error('E-mail not found. Please check you email!')) }    
-            if (user.password != password)  { reject(new Error('Your password is invalid!')) }    
+            if (user == null) { reject(new Error('E-mail not found. Please check you email!')) }
+            if (user.password != password) { reject(new Error('Your password is invalid!')) }
             resolve(user);
         });
     });
 }
+ 
+register = (user) => {
+    if (user.password != user.repassword) {
+        console.log('Password and Re-enter password do not match!')
+    } else {
+        const newUser = {
+            firstName: user.firstName,
+            surname: user.surname,
+            email: user.email,
+            password: user.password,
+            isAdmin: true
+        }
 
+        return new Promise((resolve, reject) => {
+            new User(newUser).save().then((user) => {
+                resolve(user);
+            }).catch((err) => setImmediate(() => { reject(new Error(err)) }));
+        })
+    }
+}
 
 module.exports = {
-    login
+    login,
+    register
 }
