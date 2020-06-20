29f5652308f1cfd99eb8614a9cd7f969f2c00efc
anashibata <51089103+anashibata@users.noreply.github.com>
2020-06-20 19:31:23 +0100

    Add files via upload

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,3 +3,27 @@ function callDisplayLocations() {
     urlListLocations = "window.location.href=href=" + "'"+aListLocations.join('/')+"'"
     document.getElementById("btnFilter").setAttribute('onclick', urlListLocations) //"window.location.href=href='/locations/Australia';"
 }
+
+
+function validateLocation(id) {   
+    aValidate = ['validate', 
+              id,
+              $("#"+id).attr('name')]
+    urlValidate = aValidate.join('/') 
+              
+    $.ajax({
+        url: urlValidate,
+        type: 'POST',
+        success: (data) => {  
+            validate = (data == 'true') 
+            
+            $("#"+id).attr('name', !(validate))
+            if (validate) {
+                document.getElementById(id).innerHTML='Invalidate'
+            } else {
+                document.getElementById(id).innerHTML='Validate'
+            }
+            alert('Nova location foi atribuida '+document.getElementById(name).innerHTML)
+        }     
+    })   
+}
